d4b05bfee08bbaf4420ba90e485b8a098f2b521b
Riiuzaky <jonathans.cruzm@gmail.com>
2024-02-02 17:54:24 -0500

    Import json de manera nativa

--- App.mjs
+++ App.mjs
@@ -4,6 +4,14 @@ import crypto from 'node:crypto'
 import { valdiatePartialMovie, validateMovie } from './Schemas/movies.mjs'
 import cors from 'cors'
 
+// const movies = JSON.parse(fs.readFileSync('./movies.json, utf-8))
+
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+
+const movies = require('./movies.json')
+
 const app = express()
 
 app.use(express.json())
@@ -36,8 +44,8 @@ const PORT = process.env.PORT ?? 1234
 app.get('/movies', async (req, res) => {
   try {
     const { genre } = req.query
-    const moviesBuffer = await readFile('./movies.json')
-    const movies = JSON.parse(moviesBuffer)
+    //const moviesBuffer = await readFile('./movies.json')
+    //const movies = JSON.parse(moviesBuffer)
     if (genre) {
       const filteredMovies = movies.filter((movie) =>
         movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
@@ -54,8 +62,8 @@ app.get('/movies', async (req, res) => {
 app.get('/movies/:id', async (req, res) => {
   try {
     const { id } = req.params
-    const moviesBuffer = await readFile('./movies.json')
-    const movies = JSON.parse(moviesBuffer)
+    //const moviesBuffer = await readFile('./movies.json')
+    //const movies = JSON.parse(moviesBuffer)
     const movie = movies.find((movie) => movie.id === id)
     if (movie) {
       res.json(movie)
@@ -74,14 +82,14 @@ app.post('/movies', async (req, res) => {
     return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
 
-  const moviesBuffer = await readFile('./movies.json')
-  const movies = JSON.parse(moviesBuffer)
+  //const moviesBuffer = await readFile('./movies.json')
+  //const movies = JSON.parse(moviesBuffer)
 
   const newMovie = {
     id: crypto.randomUUID(),
     ...result.data,
     // title,
-    // genre,
+    // genre, 
     // year,
     // director,
     // duration,
@@ -101,8 +109,8 @@ app.patch('/movies/:id', async (req, res) => {
   }
 
   const { id } = req.params
-  const moviesBuffer = await readFile('./movies.json')
-  const movies = JSON.parse(moviesBuffer)
+  //const moviesBuffer = await readFile('./movies.json')
+  //const movies = JSON.parse(moviesBuffer)
   const movieIndex = movies.findIndex((movie) => movie.id === id)
 
   if (movieIndex === -1) {
@@ -122,8 +130,8 @@ app.patch('/movies/:id', async (req, res) => {
 
 app.delete('/movies/:id', async (req, res) => {
   const { id } = req.params
-  const moviesBuffer = await readFile('./movies.json')
-  const movies = JSON.parse(moviesBuffer)
+  //const moviesBuffer = await readFile('./movies.json')
+  //const movies = JSON.parse(moviesBuffer)
   const movieIndex = movies.findIndex(movie => movie.id === id)
 
   if (movieIndex === -1) {
